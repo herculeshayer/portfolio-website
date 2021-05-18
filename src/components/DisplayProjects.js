6434f2d4a5eb849a4ebd9c009fb2648d1d452bba
Harman Hayer <hhhayer@gmail.com>
2021-05-18 15:49:08 -0700

    add mapping functionality & github repo data

--- src/components/DisplayProjects.js
+++ src/components/DisplayProjects.js
@@ -1,24 +1,45 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 import Data from "../data/Data";
+
+import myBlog from "../images/my-blog.jpg";
 
 const projectStyle = {
     paddingTop : 150
 }
+const imageStyle = {
+    display: "block",
+    width: "15vw",
+    height: "auto"
+}
+const flexContainer = {
+    display: "grid"
+}
 
 const DisplayProjects = () => {
 
-    const projectLink = Data.map((project) => {
-        const { text, url, description } = project;
+    const projectLink = Data.map((project, key) => {
+        const { text, url, description, stack } = project;
         return (
-            <h2>{text}</h2>
+            <div key={key} style={flexContainer}>
+                <h2>{text}</h2>
+                {/* <Link to={url}></Link> */}
+                <img style={imageStyle} src={myBlog} alt='site homepage'/>
+                <button><Link to={url}>Source Code</Link></button>
+               
+                <p>{description}</p>
+                <ul>
+                    <li>{stack}</li>
+                </ul>
+            </div>
         );
     })
     return (
-        <>
-        <h3 style={projectStyle}>Projects Here</h3>
-        {projectLink}
-        </>
+        <div>
+            <h3 style={projectStyle}>Projects</h3>
+            {projectLink}
+        </div>
     );
 }
 
