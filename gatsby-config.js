c9efb7593317afeaf2ca1b8a22885095e474bba0
Harman Hayer <hhhayer@gmail.com>
2021-05-18 15:47:10 -0700

    alter title & description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: "portfolio-website",
+    description: "Portfolio webpage showcasing development knowledge",
+    author: "harman"
   },
   plugins: [],
 };
