229217356b62b055269d0bd6463da10383231ef0
Harman Hayer <hhhayer@gmail.com>
2021-05-23 21:03:13 -0700

    bulk of personal detail & image css

--- src/components/ImageOfMe.js
+++ src/components/ImageOfMe.js
@@ -6,9 +6,9 @@ const imageStyle = {
     display: "block",
     marginLeft: "auto",
     marginRight: "auto",
-    width: "30%",
+    width: "22vw",
     height: "auto",
-    borderRadius: '10%',
+    borderRadius: '100%',
 }
 const container = {
     textAlign: 'center'
@@ -23,13 +23,27 @@ const ImageOfMe = () => {
                 src={imageOfMe}
                 alt='My Face'
             />  
-            <h4>Hello, my names Harman</h4>
-            <p>
-                I'm a budding software developer, looking for
-                my big break into the industry. <br/>
-                This is my portfolio webpage, below you'll find 
-                projects. 
-                <br/>Enjoy
+            <h1 style={{fontSize: "3vw",textShadow: "0.1vw 0.1vw #808080"}}>Hi, I'm Harman</h1>
+            <p style={{fontSize: "18px", paddingRight: "20px", paddingLeft: "20px"}}>
+                I'm a self-taught developer, I've accomplished this through
+                Lynda/LinkedInLearning, YouTube, and Udemy. I've decided to complete a
+                few projects which will hopefully demonstrate my knowledge in both Frontend 
+                and Backend software development.
+                <br/>
+                <br/>
+                I've recieved my diploma in Computer Information Systems (CIS) from Kwantlen 
+                Polytechnic University, and have since transferred my credits to Thompson River University
+                and plan to complete my 3rd and 4th years in Computing Science.
+                <br/>
+                <br/>
+                I have a yearning for knowledge & desire for success. I'm willing to learn any and all 
+                technologies which will further advance my career and passions. 
+                <br/>
+                <br/>
+                Below are a few projects which I have completed. I am in the process of completing additional projects.
+                <br/>
+                <br/>
+                Thank you, and enjoy!
             </p>
       </article>
     );
