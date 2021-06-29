71e8835112d165b43ca75f6108f11d6b32e00126
Harman Hayer <hhhayer@gmail.com>
2021-06-29 12:44:46 -0700

    additional project boilerplate

--- src/data/Data.js
+++ src/data/Data.js
@@ -3,6 +3,7 @@
 // import Barbell from "../images/barbell.svg";
 import Blog from "../images/pyre.svg";
 import portImg from "../images/desktop.svg";
+import tomato from "../images/tomato.svg";
 
 
 const links = [
@@ -31,18 +32,26 @@ const links = [
       text: "Portfolio Website",
       url: "https://github.com/herculeshayer/portfolio-website",
       liveSite: "https://hhayer.me/",
-      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery practices",
-      tools: "GatsbyJS, HTML, CSS, JavaScript",
+      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery workflows",
+      tools: "Tools: GatsbyJS, HTML, CSS, JavaScript",
       img: portImg
     },
     {
       text: "Grit & Gain: Workout Blog",
       url: "https://github.com/herculeshayer/grit-and-gain",
-      liveSite: "https://main.d3i8l79hqpk5g.amplifyapp.com/",
-      description: "A full-stack blog website hosted on AWS Amplify usng CI/CD workflows, the frontend is created using ReactJS and the backend uses NodeJS, MongoDB is the database and ExpressJS is used as the server",
-      tools: "MongoDB, ExpressJS, ReactJS, NodeJS, AWS Amplify, Git",
+      liveSite: "https://www.gritandgain.xyz/",
+      description: "A full-stack MERN blog website hosted on AWS Amplify using CI/CD workflows, the frontend is created using ReactJS and the backend uses NodeJS, MongoDB is the database and ExpressJS is used as the server",
+      tools: "Tools: MongoDB, ExpressJS, ReactJS, NodeJS, AWS Amplify, Mongo Atlas, Git, RESTful API",
       img: Blog
     },
+    {
+      text: "MiniDoro: Pomodoro Timer",
+      url: "https://github.com/herculeshayer/minidoro",
+      liveSite: "https://github.com/herculeshayer/minidoro",
+      description: "A simple browser based pomodoro timer",
+      tools: "Tools: React, JavaScript, CSS, HTML, Git",
+      img: tomato
+    }
     
   ]
 
