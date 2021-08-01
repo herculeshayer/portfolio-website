1f7a61a8fbb46b33a2e4c47a9a73e44fbd825ba7
Harman Hayer <hhhayer@gmail.com>
2021-08-01 14:34:46 -0700

    fix background color on body tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,35 +9,22 @@ import DisplayProjects from '../components/DisplayProjects';
 
 
 
-const bodyStyles = {
-  backgroundColor: "#FDF1C3",
-  // marginLeft: "auto",
-  // marginRight: "auto",
-  // textAlign: "center"
-}
 
 // styles
 const pageStyles = {
   color: "#1B2A41",
-  // padding: 96,
   fontFamily: "arial",
   fontSize: '15px'
 }
-// const paddingStyle = {
-//   paddingBottom: 100,
-//   textAlign: "center",
-//   fontSize: "8vw",
-// }
 
 
 // markup
 const IndexPage = () => {
   return (
-  <main style={bodyStyles} >
+  <main>
     
     <title>Harman's Portfolio</title>
     <article style={pageStyles}>
-      {/* <h1 style={paddingStyle}>Welcome to my portfolio!</h1> */}
       <ImageOfMe />
       <br />
       <DisplayProjects />
