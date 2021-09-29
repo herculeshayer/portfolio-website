dada7c3dc903aeaba70bac9d3c3646d28200a9f4
Harman Hayer <hhhayer@gmail.com>
2021-09-29 13:58:23 -0700

    link to email, github, and linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,14 @@ import * as React from 'react';
 
 const Footer = () => {
     return(
-        <h1>Footer</h1>
+        <footer>
+            <ul>
+                <li><a href="mailto:hhhayer@gmail.com">hhhayer@gmail.com</a></li>
+                <li><a href="https://www.github.com/herculeshayer">github.com/herculeshayer</a></li>
+                <li><a href="https://www.linkedin.com/in/harman-hayer/">linkedin.com/harmanhayer</a></li>
+
+            </ul>
+        </footer>
     );
 }
 
