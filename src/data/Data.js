b4bd747cffa97611568fd5e767656d9f7bc9f6bf
Harman Hayer <hhhayer@gmail.com>
2021-09-30 14:35:11 -0700

    array of objects containing project info

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,37 +1,51 @@
 // import HemoImage from "../images/blood-drop.svg";
 // import TODO from "../images/todo.png";
 // import Barbell from "../images/barbell.svg";
-import Blog from "../images/pyre.svg";
-import portImg from "../images/desktop.svg";
-import tomato from "../images/tomato.svg";
 
 
-const links = [
+
+const homepageLinks = [
+    
     {
-      text: "Portfolio Website",
-      url: "https://github.com/herculeshayer/portfolio-website",
-      liveSite: "https://hhayer.me/",
-      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery workflows",
-      tools: "Tools: GatsbyJS, HTML, CSS, JavaScript",
-      img: portImg
-    },
-    {
-      text: "Grit & Gain: Workout Blog",
+      text: "grit & gain",
       url: "https://github.com/herculeshayer/grit-and-gain",
       liveSite: "https://gritandgain.netlify.app/",
       description: "A full-stack MERN blog website hosted on AWS Amplify using CI/CD workflows, the frontend is created using ReactJS and the backend uses NodeJS, MongoDB is the database and ExpressJS is used as the server",
-      tools: "Tools: MongoDB, ExpressJS, ReactJS, NodeJS, AWS Amplify, Mongo Atlas, Git, RESTful API",
-      img: Blog
+      tools: "react, node, express, mongo, aws",
+      
     },
     {
-      text: "MiniDoro: Pomodoro Timer",
+      text: "minidoro",
       url: "https://github.com/herculeshayer/minidoro",
       liveSite: "https://minidoro.herokuapp.com/",
       description: "A simple browser based pomodoro timer",
-      tools: "Tools: React, JavaScript, CSS, HTML, Git",
-      img: tomato
-    }
+      tools: "html, sass, javascript, node, express, heroku",
+      
+    },
+    {
+      text: "chirpy",
+      url: "https://github.com/herculeshayer/chirpy",
+      liveSite: "https://chirpy-twitter-clone.herokuapp.com/",
+      tools: "node, express, mongodb, heroku",
+      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery workflows",
+      
+    },
+    {
+      text: "portfolio",
+      url: "https://github.com/herculeshayer/portfolio-website",
+      liveSite: "https://hhayer.me/",
+      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery workflows",
+      tools: "html, sass, gatsby, netlify",
+      
+    },
     
   ]
 
-export default links;
+const projectpageLinks = [
+  
+]
+
+export {
+  homepageLinks,
+  projectpageLinks
+}
