854b29463204d8470a5ce49be09e225bb9c6e215
Harman Hayer <hhhayer@gmail.com>
2021-09-29 21:47:33 -0700

    add mail class, and signout msg in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,12 @@ const Footer = () => {
     return(
         <footer>
             <ul>
-                <li><a href="mailto:hhhayer@gmail.com">hhhayer@gmail.com</a></li>
+                <li><a className="mail" href="mailto:hhhayer@gmail.com">hhhayer@gmail.com</a></li>
                 <li><a href="https://www.github.com/herculeshayer">github.com/herculeshayer</a></li>
                 <li><a href="https://www.linkedin.com/in/harman-hayer/">linkedin.com/harmanhayer</a></li>
 
+
+                <li id="page-end">designed + created w/ love by harman</li>
             </ul>
         </footer>
     );
