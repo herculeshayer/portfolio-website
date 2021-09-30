69335437354276bc2ee1dfac18f52d7d72e4ba7b
Harman Hayer <hhhayer@gmail.com>
2021-09-30 14:35:18 -0700

    add project page

---
+++ src/pages/projects.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+
+import Header from './../components/Header'
+
+//styles
+import './../css/index.css'
+
+const Projects = () => {
+    return(
+        <section>
+
+            <Header />
+            <NavBar />
+                <h1>Projects</h1>
+            <Footer />
+
+        </section>
+    );
+}
+
+export default Projects;
