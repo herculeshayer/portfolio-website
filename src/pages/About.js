29595232ffa46c67a30d80da46e06fb226aac664
Harman Hayer <hhhayer@gmail.com>
2021-09-30 14:34:32 -0700

    map project data to homepage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import NavBar from '../components/NavBar';
 
+
+//styles
+import './../css/index.css'
 
 const About = () => {
     return(
-        <h1>About</h1>
+        <section>
+            <Header />
+            <NavBar />
+                <h1>About</h1>
+            <Footer />
+
+
+        </section>
     );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,39 @@
 import * as React from 'react';
 
+import { homepageLinks } from './../data/Data';
 
 const Home = () => {
     return(
-        <section className="home-flex-container">
+        <article id="grid">
 
-            <div className="home-flex-item">
-                Short Intro
+            <div id="grid-bio-item">
+                <span><span id="hi">hi!</span> my names harman and i'm an aspiring software developer</span>
+                <p>i have created a few full stack applications currently hosted on several cloud providers</p>
+                <p>have a look around and stay a while!</p>
             </div>
-            <div className="home-flex-item">
-                Projects
-            </div>
+            <div id="grid-project">
 
-        </section>
+                {
+                    homepageLinks.map((project, key) => {
+                        const { text, url, tools } = project;
+                        return(
+                            <div id="grid-project-item">
+                                <a href={url}>
+                                    <h1>{text}</h1>
+                                    <p>{tools}</p>
+
+                                </a>
+
+                            </div>
+                        );
+                    })
+                }
+                
+            </div>
+            
+            
+
+        </article>
     );
 }
 
