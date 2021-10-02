3cd52ba755210af4d469bb77ca5bdd254ce23904
Harman Hayer <hhhayer@gmail.com>
2021-10-02 09:20:09 -0700

    update resume to link to s3 bucket

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
         <nav>
             <ul>
                 <li><Link to="/">home</Link></li>
-                <li><a href="https://resume.creddle.io/resume/47zeay722f2">resume</a></li>
+                <li><a href="https://pdf-host-bucket.s3.us-west-2.amazonaws.com/resume-v7.pdf">resume</a></li>
                 <li><Link to="/projects">projects</Link></li>
                 <li><Link to="/about">about</Link></li>
             </ul>
