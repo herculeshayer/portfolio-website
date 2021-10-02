f0e59eeaf1ec6318a387b029b8db290a66e75c57
Sarthak Nirala <sarthaknirala2509@gmail.com>
2021-10-03 00:19:29 +0530

    Added pull transition on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
     return(
         <nav>
             <ul>
-                <li><Link to="/">home</Link></li>
-                <li><a href="https://resume.creddle.io/resume/47zeay722f2">resume</a></li>
-                <li><Link to="/projects">projects</Link></li>
-                <li><Link to="/about">about</Link></li>
+                <li class ="grow"><Link to="/">home</Link></li>
+                <li class ="grow"><a href="https://resume.creddle.io/resume/47zeay722f2">resume</a></li>
+                <li class ="grow"><Link to="/projects">projects</Link></li>
+                <li class ="grow"><Link to="/about">about</Link></li>
             </ul>
         </nav>
     );
