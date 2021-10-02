2897b2f96cef1a0de3024e988c91f46a6c221c83
Harman Hayer <hhhayer@gmail.com>
2021-10-02 09:25:40 -0700

    add project data

--- src/data/Data.js
+++ src/data/Data.js
@@ -9,40 +9,70 @@ const homepageLinks = [
     {
       text: "grit & gain",
       url: "https://github.com/herculeshayer/grit-and-gain",
-      liveSite: "https://gritandgain.netlify.app/",
-      description: "A full-stack MERN blog website hosted on AWS Amplify using CI/CD workflows, the frontend is created using ReactJS and the backend uses NodeJS, MongoDB is the database and ExpressJS is used as the server",
       tools: "react, node, express, mongo, aws",
       
     },
     {
       text: "minidoro",
       url: "https://github.com/herculeshayer/minidoro",
-      liveSite: "https://minidoro.herokuapp.com/",
-      description: "A simple browser based pomodoro timer",
       tools: "html, sass, javascript, node, express, heroku",
       
     },
     {
       text: "chirpy",
       url: "https://github.com/herculeshayer/chirpy",
-      liveSite: "https://chirpy-twitter-clone.herokuapp.com/",
       tools: "node, express, mongodb, heroku",
-      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery workflows",
       
     },
     {
       text: "portfolio",
       url: "https://github.com/herculeshayer/portfolio-website",
-      liveSite: "https://hhayer.me/",
-      description: "A basic GatsbyJS webpage hosted on Netlify using Continuous Integration/Continuous Delivery workflows",
       tools: "html, sass, gatsby, netlify",
-      
     },
     
   ]
 
 const projectpageLinks = [
+  {
+    text: "grit & gain",
+    source: "https://github.com/herculeshayer/grit-and-gain",
+    live: "https://gritandgain.netlify.app/",
+    description: "a full-stack mern blog website hosted on aws amplify using ci/cd workflows, the frontend is created using reactjs and the backend uses nodejs, mongodb is the database and expressjs is used as the server",
+    tools: "react, node, express, mongo, aws",
+    
+  },
+  {
+    text: "minidoro",
+    source: "https://github.com/herculeshayer/minidoro",
+    live: "https://minidoro.herokuapp.com/",
+    description: "a full stack application created using javascript. users can register, login, and run + track their pomodoros(work sessions)",
+    tools: "html, sass, javascript, node, express, jwt, heroku",
+    
+  },
+  {
+    text: "chirpy",
+    source: "https://github.com/herculeshayer/chirpy",
+    live: "https://chirpy-twitter-clone.herokuapp.com/",
+    description: "backend twitter clone using javascript. users can register, login, tweet, and chat with others",
+    tools: "node, express, express-sessions, mongodb, heroku",
+    
+  },
+  {
+    text: "cinemax",
+    source: "https://github.com/herculeshayer/CineMax",
+    live: "",
+    description: "a mobile application created using java. users can register for an account, login, select movie tickets, concessions, seating, and manage their account",
+    tools: "java, javafx, linux, apache, mysql",
+  },
+  {
+    text: "portfolio",
+    source: "https://github.com/herculeshayer/portfolio-website",
+    live: "https://hhayer.me/",
+    description: "a basic gatsbyjs webpage hosted on netlify using continuous integration/continuous delivery workflows",
+    tools: "html, sass, gatsby, netlify",
+  },
   
+
 ]
 
 export {
