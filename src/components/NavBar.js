ff9e659bf9fb9a6d7cce5306d513d55820e41e69
Sarthak88221 <80682644+Sarthak88221@users.noreply.github.com>
2021-10-05 07:57:31 +0530

    Added pull transition on navbar (#11)
    Co-authored-by: harman <hhhayer@gmail.com>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,12 @@ const NavBar = () => {
     return(
         <nav>
             <ul>
-                <li><Link to="/">home</Link></li>
-                <li><a href="https://pdf-host-bucket.s3.us-west-2.amazonaws.com/resume-v7.pdf">resume</a></li>
-                <li><Link to="/projects">projects</Link></li>
-                <li><Link to="/aboutme">about</Link></li>
+
+                <li class="grow"><Link to="/">home</Link></li>
+                <li class="grow"><a href="https://pdf-host-bucket.s3.us-west-2.amazonaws.com/resume-v7.pdf">resume</a></li>
+                <li class="grow"><Link to="/projects">projects</Link></li>
+                <li class="grow"><Link to="/aboutme">about</Link></li>
+
             </ul>
         </nav>
     );
