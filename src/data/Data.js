b4905a247f9b84a3312b36451b4ffdef7eee0ec7
Harman Hayer <hhhayer@gmail.com>
2021-11-14 19:22:38 -0800

    fix: add data

--- src/data/Data.js
+++ src/data/Data.js
@@ -35,16 +35,14 @@ const projectpageLinks = [
     source: "https://github.com/herculeshayer/treeeeee",
     live: "https://gritandgain.netlify.app/",
     description: "a full-stack e-commerce website hosted on aws amplify & aws beanstalk. the frontend is created using react and the backend uses java spring boot, and postgresql",
-    tools: "react, sass, java, spring boot, aws",
-    
+    tools: "react, sass, java, spring boot, postgresql, aws",
   },
   {
     text: "minidoro",
     source: "https://github.com/herculeshayer/minidoro",
     live: "https://minidoro.herokuapp.com/",
     description: "a full stack application created using node and postgresql. users have access to register, login, and work session tracking functionality",
-    tools: "html, sass, javascript, node, express, jwt, postgresql, heroku",
-    
+    tools: "html, sass, javascript, node, express, jwt, postgresql, heroku", 
   },
   {
     text: "airdex",
@@ -52,7 +50,6 @@ const projectpageLinks = [
     live: "https://airdex.netlify.app/",
     description: "frontend react app utilizing an external api for users to search and display air quality index forecasts",
     tools: "react, sass, chartjs, git, aws, netlify",
-    
   },
   {
     text: "chirpy",
@@ -60,7 +57,6 @@ const projectpageLinks = [
     live: "https://chirpy-twitter-clone.herokuapp.com/",
     description: "backend twitter clone using javascript. users can register, login, tweet, and chat with others",
     tools: "node, express, express-sessions, mongodb, heroku",
-    
   },
   {
     text: "grit & gain",
@@ -68,7 +64,6 @@ const projectpageLinks = [
     live: "https://gritandgain.netlify.app/",
     description: "a full-stack mern blog website hosted on aws utilizing ci/cd. the frontend is created using react and the backend uses node, express, and mongodb",
     tools: "react, node, express, mongodb + mongodb atlas, aws",
-    
   },
   {
     text: "portfolio",
