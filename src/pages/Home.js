4d56a3773ca9a833dcfd1b72d23f87edeb34ecf8
harman <hhhayer@gmail.com>
2023-03-14 12:55:52 -0700

    rm about page and childish spirit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
         <article id="grid">
 
             <div id="grid-bio-item">
-                <span id="hi">bonjour!</span>
-                <div>as long as you live, keep learning how to live - <b>seneca</b></div>
-                <p>your time is limited, so dont waste it living someone elses life - <b>steve jobs</b></p>
-                <p><a href="https://www.youtube.com/watch?v=Hj0UvmXrQC0">thanks for stopping by!</a></p>
+                <span id="hi">hello!</span>
+                <div>here you'll find a few projects of mine with their description & technologies used</div>
+                <p>projects are created mainly for my own curiosity & growth</p>
+                <p>thanks for stopping by! </p>
             </div>
             <div id="grid-project">
 
