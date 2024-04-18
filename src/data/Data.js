a81e74816305c6dbd64c6be9dc3417351f184684
Harman <hhhayer@gmail.com>
2024-04-17 18:58:02 -0700

    update project info and update dependencies

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,87 +1,98 @@
-
 const homepageLinks = [
-  {
-    text: "peak",
-    url: "https://github.com/herculeshayer/peak",
-    tools: "swift, swiftui, xcode, openai",
-  },
-  {
-    text: "vehicle speed detection",
-    url: "https://github.com/herculeshayer/computer-vision-opencv",
-    tools: "python, opencv, aws",
-  },
-  {
-    text: "minidoro",
-    url: "https://github.com/herculeshayer/minidoro",
-    tools: "html, sass, react, node, express, jwt, postgresql, heroku",
-  },
-  {
-    text: "airdex",
-    url: "https://github.com/herculeshayer/airdex",
-    tools: "react, sass, chartjs, aws, netlify",
-  }, 
-]
+    {
+        text: "peak fitness app",
+        url: "https://github.com/peak-fitness-app",
+        tools: "java (android), swift(ios), swiftui, openai",
+    },
+    {
+        text: "vehicle speed detection",
+        url: "https://github.com/herculeshayer/computer-vision-opencv",
+        tools: "python, opencv, ultralytics, aws",
+    },
+    {
+        text: "minidoro",
+        url: "https://github.com/herculeshayer/minidoro",
+        tools: "react, html, sass, node, express, jwt, postgresql, netlify, render, supabase",
+    },
+    {
+        text: "airdex",
+        url: "https://github.com/herculeshayer/airdex",
+        tools: "react, sass, chartjs, aws, netlify",
+    },
+];
 
 const projectpageLinks = [
-  {
-    text: "peak",
-    source: "https://github.com/herculeshayer/peak",
-    live: "https://gritandgain.netlify.app/",
-    description: "simple mobile workout app utilizing openai api",
-    tools: "swift, swiftui, xcode, openai",
-  },
-  {
-    text: "computer vision object speed detection",
-    source: "https://github.com/herculeshayer/computer-vision-opencv",
-    live: "https://gritandgain.netlify.app/",
-    description: "object detection application, view object speed and alert if above threshold",
-    tools: "python, opencv, aws",
-  },
-  {
-    text: "minidoro",
-    source: "https://github.com/herculeshayer/minidoro",
-    live: "https://minidoro.herokuapp.com/",
-    description: "a full-stack application created using react, node, and postgresql. users have access to register, login, and work session tracking functionality",
-    tools: "html, scss, react, node, express, jwt, aws rds, render, netlify", 
-  },
-  {
-    text: "airdex",
-    source: "https://github.com/herculeshayer/airdex",
-    live: "https://airdex.netlify.app/",
-    description: "frontend react app utilizing an external api for users to search and display air quality index forecasts",
-    tools: "react, scss, chartjs, git, aws, netlify",
-  },
-  {
-    text: "chirpy",
-    source: "https://github.com/herculeshayer/chirpy",
-    live: "https://chirpy-twitter-clone.herokuapp.com/",
-    description: "backend twitter clone using javascript. users can register, login, tweet, and chat with others",
-    tools: "node, express, express-sessions, mongodb, heroku",
-  },
-  {
-    text: "grit & gain",
-    source: "https://github.com/herculeshayer/grit-and-gain",
-    live: "https://gritandgain.netlify.app/",
-    description: "a full-stack mern blog website hosted on aws utilizing ci/cd. the frontend is created using react and the backend uses node, express, and mongodb",
-    tools: "html, css, react, node, express, mongodb + mongodb atlas, aws",
-  },
-  {
-    text: "portfolio",
-    source: "https://github.com/herculeshayer/portfolio-website",
-    live: "https://hhayer.me/",
-    description: "a portfolio webpage created using gatsby and hosted on netlify",
-    tools: "html, scss, gatsby, netlify",
-  },
-  {
-    text: "cinemax",
-    source: "https://github.com/herculeshayer/CineMax",
-    live: "",
-    description: "an application created using java and javafx. user data is stored using mysql. users can register for an account, login, select movie tickets, concessions, seating, and manage their account",
-    tools: "java, javafx, linux, apache, mysql",
-  },
-]
+    {
+        text: "peak fitness app ",
+        source: "https://github.com/herculeshayer/peak",
+        live: "https://gritandgain.netlify.app/",
+        description:
+            "native android & ios minimalistic mobile workout app utilizing openai api",
+        tools: "android, swift, swiftui, openai",
+    },
+    {
+        text: "alienator",
+        source: "https://github.com/herculeshayer/alienator",
+        live: "https://gritandgain.netlify.app/",
+        description: "data visualization using dataset from api ",
+        tools: "typescript, nextjs, vercel",
+    },
+    {
+        text: "computer vision object speed detection",
+        source: "https://github.com/herculeshayer/computer-vision-opencv",
+        live: "https://gritandgain.netlify.app/",
+        description:
+            "object detection application, view object speed and alert if above threshold",
+        tools: "python, opencv, aws",
+    },
+    {
+        text: "minidoro",
+        source: "https://github.com/herculeshayer/minidoro",
+        live: "https://minidoro.herokuapp.com/",
+        description:
+            "a full-stack application created using react, node, and postgresql. users have access to register, login, and work session tracking functionality",
+        tools: "react, html, sass, node, express, jwt, postgresql, netlify, render, supabase",
+    },
+    {
+        text: "airdex",
+        source: "https://github.com/herculeshayer/airdex",
+        live: "https://airdex.netlify.app/",
+        description:
+            "frontend react app utilizing an external api for users to search and display air quality index forecasts",
+        tools: "react, scss, chartjs, git, aws, netlify",
+    },
+    {
+        text: "chirpy",
+        source: "https://github.com/herculeshayer/chirpy",
+        live: "https://chirpy-twitter-clone.herokuapp.com/",
+        description:
+            "backend twitter clone using javascript. users can register, login, tweet, and chat with others",
+        tools: "node, express, express-sessions, mongodb, heroku",
+    },
+    {
+        text: "grit & gain",
+        source: "https://github.com/herculeshayer/grit-and-gain",
+        live: "https://gritandgain.netlify.app/",
+        description:
+            "a full-stack mern blog website hosted on aws utilizing ci/cd. the frontend is created using react and the backend uses node, express, and mongodb",
+        tools: "html, css, react, node, express, mongodb + mongodb atlas, aws",
+    },
+    {
+        text: "portfolio",
+        source: "https://github.com/herculeshayer/portfolio-website",
+        live: "https://hhayer.me/",
+        description:
+            "a portfolio webpage created using gatsby and hosted on netlify",
+        tools: "html, scss, gatsby, netlify",
+    },
+    {
+        text: "cinemax",
+        source: "https://github.com/herculeshayer/CineMax",
+        live: "",
+        description:
+            "an application created using java and javafx. user data is stored using mysql. users can register for an account, login, select movie tickets, concessions, seating, and manage their account",
+        tools: "java, javafx, linux, apache, mysql",
+    },
+];
 
-export {
-  homepageLinks,
-  projectpageLinks
-}
+export { homepageLinks, projectpageLinks };
