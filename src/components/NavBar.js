8d21ce2cf67678a27ea0629ce4a038e786c6fd4b
chimp <hhhayer@gmail.com>
2024-08-15 20:53:47 -0700

    change resume url to new aws acc

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,25 @@
-import * as React from 'react';
-import { Link } from 'gatsby'
+import * as React from "react";
+import { Link } from "gatsby";
 
 const NavBar = () => {
-    return(
-        <nav>
-            <ul>
-
-                <li class="grow"><Link to="/">home</Link></li>
-                <li class="grow"><a href="https://resume-v1.s3.us-west-2.amazonaws.com/resume-v1.51.pdf">resume</a></li>
-                <li class="grow"><Link to="/projects">projects</Link></li>
-                {/* <li class="grow"><Link to="/aboutme">about</Link></li> */}
-
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav>
+      <ul>
+        <li class="grow">
+          <Link to="/">home</Link>
+        </li>
+        <li class="grow">
+          <a href="https://hhayer-resume.s3.us-west-2.amazonaws.com/resume-v1.51.pdf">
+            resume
+          </a>
+        </li>
+        <li class="grow">
+          <Link to="/projects">projects</Link>
+        </li>
+        {/* <li class="grow"><Link to="/aboutme">about</Link></li> */}
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
